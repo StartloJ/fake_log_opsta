2d221de06771b890d127d4b04de96e8fba27499e
startloj <lord.tart@gmail.com>
2019-02-12 10:43:58 +0700

    edit parse args and add md

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# fake_log_opsta
+# faker_log
+
+Python generate log from source log file in single line only and flush rate 1 message / sec (you can change rate in DURATION line 3)
+
+## Option in script
+<pre>
+usage: fake_log.py [-h] [-t] [--thread] [--text TEXT] [-l] [-d DESTFILE]
+                   [-f FILTER] [--timeformat TIMEFORMAT]
+                   path_to_source_log_file
+
+positional arguments:
+  path_to_source_log_file               insert path to source log file like './logs/action.log'
+
+optional arguments:
+  -h, --help                            show this help message and exit
+  -a, --all_default                     this option to use default log format is [ asctime [threadName] levelname message ]
+  --text TEXT                           this option to insert sometext in new log file
+  -d DESTFILE, --destfile DESTFILE      this option to store new log file to your path
+  -f FILTER, --filter FILTER            this option for edit regex filter source log to short messages such as '.*' .
+  --logformat LOGFORMAT                 this option for edit log-header format without message body to generate .
+  --timeformat TIMEFORMAT               this option for edit timestamp format ref. in [http://strftime.org] .
+</pre>
+
+## Explain log format to use in [--logformat]
+* asctime => %(asctime)s : is datetime is python get from system.
+* threadName => %(threadName)s : is python get from complier thread default is mainthread.
+* levelname => %(levelname)s : is python logging generate from log level to flush.
+
+## Default values for some parameter
+-a , --all_default : True
+-d , --destfile    : "./app.log"
+-f , --filter      : "\s-\s.*"
+--logformat        : "%(asctime)s [%(threadName)s] %(levelname)s "
+--timeformat       : "%Y-%m-%d %H:%M:%S "
+
+## Sample command
+command to gen log
+
++ sample1.log with default format in parameter -a.\
+`
+    python fake_log.py path/to/file.log -a --timeformat "%d %b %Y %H:%M:%S "
+`
++ sample.log with change log format in --logformat.\
+`
+    python fake_log.py path/to/file.log --logformat "%(asctime)s %(levelname)s [%(threadName)s]" --timeformat "%d %b %Y %H:%M:%S "
+`
++ file.csv with filter by regex string.\
+`
+    python fake_log.py path/to/file.csv -f ',\w.*' --logformat "%(asctime)s" --timeformat "%a %b %d %H:%M:%S %Y ICT"
+`
+
++ file.something with insert text between header and message.\
+`
+    python fake_log.py path/to/file.stdOut.4 --logformat "%(asctime)s " --timeformat "%d %b %Y %H:%M:%S " --text "- [Thread[Thread-64,5,main]] - 4891379771 [Thread-8] DEBUG mailagent  " --filter "“\s\s-\s.*”"
+`
--- fake_log.py
+++ fake_log.py
@@ -1,22 +1,27 @@
 import logging , time , re , random
 import argparse
+DURATION = 1 # second
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("path_to_source_log_file" , type=str ,help="insert path to source log file like './logs/action.log' .")
-parser.add_argument("-t","--timestamp"  , help="this option to insert timestamp in new log file" , action="store_true")
-parser.add_argument("--thread"  , help="this option to insert threadname in new log file" , action="store_true")
-parser.add_argument("-l","--loglevel"  , help="this option to insert loglevel in new log file" , action="store_true")
-parser.add_argument("-d","--destfile"  , help="this option to store new log file to your path" , default="./app.log")
+parser.add_argument("-a","--all_default" , help="this option to use default log format is -[ asctime [threadName] levelname  message ]-" , action="store_true")
+parser.add_argument("--text" , help="this option to insert sometext in new log file" , type=str)
+parser.add_argument("-d","--destfile" , help="this option to store new log file to your path" , default="./app.log")
+parser.add_argument("-f","--filter" , help="this option for edit regex filter source log to short messages such as '.*' ." , default="\s-\s.*")
+parser.add_argument("--logformat" , help="this option for edit log-header format without message body to generate ." , default="%(asctime)s [%(threadName)s] %(levelname)s ")
+parser.add_argument("--timeformat" , help="this option for edit timestamp format ref. in [http://strftime.org] ." , default="%Y-%m-%d %H:%M:%S ")
+
 args = parser.parse_args()
-if args.timestamp and args.thread and args.loglevel:
-    logging.basicConfig(filename=args.destfile, filemode='a', level=logging.DEBUG , format='%(asctime)s [%(threadName)s] [%(levelname)s]  %(message)s')
-else:
+if not args.all_default:
     log_format = ''
-    if args.timestamp: log_format += '%(asctime)s '
-    if args.thread: log_format += '[%(threadName)s] '
-    if args.loglevel: log_format += '[%(levelname)s]'
+    log_format += args.logformat
+    if args.text: log_format += args.text
     log_format += '%(message)s'
-    logging.basicConfig(filename=args.destfile, filemode='a', level=logging.DEBUG , format=log_format)
+    logging.basicConfig(filename=args.destfile, filemode='a', level=logging.DEBUG , format=log_format,datefmt=args.timeformat)
+else:
+    logging.basicConfig(filename=args.destfile, filemode='a', level=logging.DEBUG , format="%(asctime)s [%(threadName)s] %(levelname)s  %(message)s",datefmt=args.timeformat)
+
 #path = "./docushare/Monitor.log"
 
 def is_somealpha(text):
@@ -42,14 +47,14 @@ def formatting(logms,abnormal=False):
 def insert_ms():
     with open(args.path_to_source_log_file , "r") as f:
         text = f.read().split("\n")
-    tmp = formatting([re.findall(' - .*',t) for t in text])
+    tmp = formatting([re.findall(args.filter , t) for t in text])
     if not tmp: return formatting(text,abnormal=True)
     return tmp
 
 def wr_log(logs_custom):
     while True:
         try:
-            time.sleep(1)
+            time.sleep(DURATION)
             thislog = random.choice(logs_custom)
             print("[+] write log ["+ thislog + "].")
             logging.warning(thislog)
